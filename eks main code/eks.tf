01105a3d0fb3fc45bdbae8c5cc7b56c6ddb4fdd2
kokareamol <115057222+kokareamol@users.noreply.github.com>
2024-07-12 22:41:52 +0530

    Update eks.tf

--- eks main code/eks.tf
+++ eks main code/eks.tf
@@ -92,7 +92,7 @@ resource "aws_eks_node_group" "example" {
   scaling_config {
     desired_size = 1
     max_size     = 2
-    min_size     = 1
+    min_size     = 2
   }
   instance_types = ["t2.medium"]
 
