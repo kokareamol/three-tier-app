5ca8013935b8e32e17dd823303cfc451f3d1f7d1
kokareamol <115057222+kokareamol@users.noreply.github.com>
2024-07-12 22:53:55 +0530

    Update eks.tf
    changed min and max node group size

--- eks main code/eks.tf
+++ eks main code/eks.tf
@@ -100,9 +100,9 @@ resource "aws_eks_node_group" "example" {
   scaling_config {
     desired_size = 1
     max_size     = 2
-    min_size     = 2
+    min_size     = 1
   }
-  instance_types = ["t2.medium"]
+  instance_types = ["t2.micro"]
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
